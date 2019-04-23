3ec10cd7c0548400562edff7733d42e7c81c44f5
Preston Sergent (RIT Student) <pws1453@boo-1540-16.main.ad.rit.edu>
2019-04-23 00:26:43 -0400

    Done in booth lab for tonight

--- gather_files.py
+++ gather_files.py
@@ -4,5 +4,14 @@ Practicing using subprocess to process system variables
 
 """
 import subprocess
+import cmd
+
+Class fileshell(cmd.Cmd):
+    
+
+
+
+
+
 name = subprocess.check_output(["ls","-l"])
 print(name.decode('utf8'))
